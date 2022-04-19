661eb1eea809acc8fe1c66ec49f84565dbd3b508
kadirkaraharman <49474819+kadirkaraharman@users.noreply.github.com>
2022-04-19 20:37:25 +0300

    Add files via upload

---
+++ numberguess.py
@@ -0,0 +1,36 @@
+import random
+
+top_range = input("Type a number: ")
+
+if top_range.isdigit():
+   top_range = int(top_range)
+
+   if top_range <=0:
+      print("Please type a number larger than 0 next time.")
+      quit()
+
+random_number = random.randint(0,top_range)
+
+guesses = 1 #How many times user tried to guess
+
+while True:
+    user_guess = input("Make a guess: ")
+   
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number larger than 0 next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it! ",guesses," times you tried to find")
+        break
+    elif user_guess > random_number:
+        print("You were above the number! ")
+    else:
+        print("You were below the number!")
+        
+
+    guesses +=1
+
+
